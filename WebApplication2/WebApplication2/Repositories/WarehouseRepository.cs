556f8afc7a1601510f09747fa787879dbae1f799
s27014 <s27014@pjwstk.edu.pl>
2024-05-12 18:35:55 +0200

    1 commit

---
+++ WebApplication2/WebApplication2/Repositories/WarehouseRepository.cs
@@ -0,0 +1,164 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication2.Repositories;
+
+public interface IWarehouseRepository
+{
+    public Task<int?> RegisterProductInWarehouseAsync(int idWarehouse, int idProduct, int idOrder, DateTime createdAt);
+    public Task RegisterProductInWarehouseByProcedureAsync(int idWarehouse, int idProduct, DateTime createdAt);
+    Task<bool> CheckIfProductExists(int dtoIdProduct);
+    Task<bool> CheckIfWarehouseExists(int dtoIdWarehouse);
+    Task<bool> CheckIfOrderExists(int dtoIdProduct,int dtoAmount);
+    Task<bool> CheckIfOrderCompleted(int dtoIdProduct, int dtoIdWarehouse,int dtoAmount);
+}
+
+public class WarehouseRepository : IWarehouseRepository
+{
+    private readonly IConfiguration _configuration;
+    public WarehouseRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<bool> CheckIfProductExists(int dtoIdProduct)
+    {
+        await using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        await connection.OpenAsync();
+        
+        await using var transaction = await connection.BeginTransactionAsync();
+        try
+        {
+            var query = "SELECT TOP 1 IdProduct FROM Product WHERE @idProduct = IdProduct";
+            await using var cmd = new SqlCommand(query, connection);
+            cmd.Transaction = (SqlTransaction)transaction;
+            cmd.Parameters.AddWithValue("@idProduct", dtoIdProduct);
+            var result = await cmd.ExecuteNonQueryAsync();
+
+            return result != null;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> CheckIfWarehouseExists(int dtoIdWarehouse)
+    {
+        await using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        await connection.OpenAsync();
+        
+        await using var transaction = await connection.BeginTransactionAsync();
+        try
+        {
+            var query = "SELECT TOP 1 IdWarehouse FROM Warehouse WHERE @idWarehouse = IdWarehouse";
+            await using var cmd = new SqlCommand(query, connection);
+            cmd.Transaction = (SqlTransaction)transaction;
+            cmd.Parameters.AddWithValue("@idWarehouse", dtoIdWarehouse);
+            var result = await cmd.ExecuteNonQueryAsync();
+
+            return result != null;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> CheckIfOrderExists(int dtoIdProduct, int dtoAmount)
+    {
+        await using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        await connection.OpenAsync();
+        
+        await using var transaction = await connection.BeginTransactionAsync();
+        try
+        {
+            var query = "SELECT TOP 1 IdProduct FROM Order WHERE @idProduct = IdProduct AND @Amount = Amount";
+            await using var cmd = new SqlCommand(query, connection);
+            cmd.Transaction = (SqlTransaction)transaction;
+            cmd.Parameters.AddWithValue("@idProduct", dtoIdProduct);
+            cmd.Parameters.AddWithValue("@Amount", dtoAmount);
+            var result = await cmd.ExecuteNonQueryAsync();
+
+            return result != null;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> CheckIfOrderCompleted(int dtoIdProduct, int dtoIdWarehouse,int dtoAmount)
+    {
+        await using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        await connection.OpenAsync();
+        
+        await using var transaction = await connection.BeginTransactionAsync();
+        try
+        {
+            var query = "SELECT TOP 1 IdOrder FROM Product_Warehouse WHERE @idProduct = IdProduct AND @Amount = Amount AND @idWarehouse = IdWarehouse";
+            await using var cmd = new SqlCommand(query, connection);
+            cmd.Transaction = (SqlTransaction)transaction;
+            cmd.Parameters.AddWithValue("@idProduct", dtoIdProduct);
+            cmd.Parameters.AddWithValue("@Amount", dtoAmount);
+            cmd.Parameters.AddWithValue("@idWarehouse", dtoIdWarehouse);
+            var result = await cmd.ExecuteNonQueryAsync();
+
+            return result != null;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task<int?> RegisterProductInWarehouseAsync(int idWarehouse, int idProduct, int idOrder, DateTime createdAt)
+    {
+        await using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        await connection.OpenAsync();
+        
+        await using var transaction = await connection.BeginTransactionAsync();
+
+        try
+        {
+            var query = "UPDATE \"Order\" SET FulfilledAt = @FulfilledAt WHERE IdOrder = @IdOrder";
+            await using var command = new SqlCommand(query, connection);
+            command.Transaction = (SqlTransaction)transaction;
+            command.Parameters.AddWithValue("@IdOrder", idOrder);
+            command.Parameters.AddWithValue("@FulfilledAt", DateTime.UtcNow);
+            await command.ExecuteNonQueryAsync();
+            
+            command.CommandText = @"
+                      INSERT INTO Product_Warehouse (IdWarehouse, IdProduct, IdOrder, CreatedAt, Amount, Price)
+                      OUTPUT Inserted.IdProductWarehouse
+                      VALUES (@IdWarehouse, @IdProduct, @IdOrder, @CreatedAt, 0, 0);";
+            command.Parameters.Clear();
+            command.Parameters.AddWithValue("@IdWarehouse", idWarehouse);
+            command.Parameters.AddWithValue("@IdProduct", idProduct);
+            command.Parameters.AddWithValue("@IdOrder", idOrder);
+            command.Parameters.AddWithValue("@CreatedAt", createdAt);
+            var idProductWarehouse = (int)await command.ExecuteScalarAsync();
+
+            await transaction.CommitAsync();
+            return idProductWarehouse;
+        }
+        catch
+        {
+            await transaction.RollbackAsync();
+            return null;
+        }
+    }
+    
+    public async Task RegisterProductInWarehouseByProcedureAsync(int idWarehouse, int idProduct, DateTime createdAt)
+    {
+        await using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        await connection.OpenAsync();
+        await using var command = new SqlCommand("AddProductToWarehouse", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("IdProduct", idProduct);
+        command.Parameters.AddWithValue("IdWarehouse",idWarehouse);
+        command.Parameters.AddWithValue("Amount", 0);
+        command.Parameters.AddWithValue("CreatedAt", createdAt);
+        await command.ExecuteNonQueryAsync();
+    }
+}
---
+++ WebApplication2/WebApplication2/Services/WarehouseService.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication2.Dto;
+using WebApplication2.Exceptions;
+using WebApplication2.Repositories;
+
+namespace WebApplication2.Services;
+
+public interface IWarehouseService
+{
+    public Task<int> RegisterProductInWarehouseAsync(RegisterProductInWarehouseRequestDTO dto);
+}
+
+public class WarehouseService : IWarehouseService
+{
+    private readonly IWarehouseRepository _warehouseRepository;
+    
+    public WarehouseService(IWarehouseRepository warehouseRepository)
+    {
+        _warehouseRepository = warehouseRepository;
+    }
+    
+    public async Task<int> RegisterProductInWarehouseAsync(RegisterProductInWarehouseRequestDTO dto)
+    {
+        var productExists = await _warehouseRepository.CheckIfProductExists(dto.IdProduct.Value);
+        if (!productExists)
+            throw new NotFoundException("Product with the provided identifier does not exist.");
+        
+        var warehouseExists = await _warehouseRepository.CheckIfWarehouseExists(dtoIdWarehouse: dto.IdWarehouse.Value);
+        if (!warehouseExists)
+            throw new NotFoundException("Warehouse with the provided identifier does not exist.");
+        
+        if (dto.Amount <= 0)
+            throw new ValidationException("Amount must be greater than 0.");
+
+        var orderExists = await _warehouseRepository.CheckIfOrderExists(dtoIdProduct:dto.IdProduct.Value,dtoAmount: dto.Amount.Value);
+        if (!orderExists)
+            throw new NotFoundException("Order with the provided identifier does not exist.");
+
+        var chceckIfInProductWarehouse =
+            await _warehouseRepository.CheckIfOrderCompleted(dtoIdProduct:dto.IdProduct.Value, dtoIdWarehouse: dto.IdWarehouse.Value,
+                dtoAmount:dto.Amount.Value);
+        if (chceckIfInProductWarehouse)
+            throw new NotFoundException("Order with procided identifier already exists");
+        
+        const int idOrder = 1;
+
+        var idProductWarehouse = await _warehouseRepository.RegisterProductInWarehouseAsync(
+            idWarehouse: dto.IdWarehouse!.Value,
+            idProduct: dto.IdProduct!.Value,
+            idOrder: idOrder,
+            createdAt: DateTime.UtcNow);
+
+        if (!idProductWarehouse.HasValue)
+            throw new Exception("Failed to register product in warehouse");
+
+        return idProductWarehouse.Value;
+    }
+}
